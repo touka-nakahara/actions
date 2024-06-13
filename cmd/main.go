dfc62946d6a52dd870ad447d2f3996ffbee4d6a0
touka-nakahara <167406123+touka-nakahara@users.noreply.github.com>
2024-06-13 16:53:50 +0900

    test for actions

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"testing"
 )
+
+func TestNewEqual(t *testing.T) {
+	if errors.New("abc") == errors.New("abc") {
+		t.Errorf(`New("abc") == New("abc")`)
+	}
+}
 
 func main() {
 	fmt.Println("GitHubActions")
-															fmt.Print("Heelo GitHub Aciotns\n")
+	fmt.Print("Heelo GitHub Aciotns\n")
 }

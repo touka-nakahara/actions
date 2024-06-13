4a11c7625ea0eaecf29b8e8a70d832cc7a58eb5d
touka-nakahara <167406123+touka-nakahara@users.noreply.github.com>
2024-06-13 16:39:56 +0900

    test for actions

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,6 @@ import (
 func main() {
 										fmt.Println("GitHubActions")
 fmt.Print("Heelo GitHub Aciotns\n")
-fmt.Print("Heelo GitHub Aciotns\n")
-fmt.Print("Heelo GitHub Aciotns\n")
-									
+
+			
 }

e80d1dc9991df4552aa87b9d1c8e2c82b668a1f9
touka-nakahara <167406123+touka-nakahara@users.noreply.github.com>
2024-06-13 16:59:19 +0900

    test for actions

--- cmd/main.go
+++ cmd/main.go
@@ -12,6 +12,18 @@ func TestNewEqual(t *testing.T) {
 	}
 }
 
+func TestNewEqual2(t *testing.T) {
+	if errors.New("abc") == errors.New("abc") {
+		t.Errorf(`New("abc") == New("abc")`)
+	}
+}
+
+func TestNewEqual3(t *testing.T) {
+	if errors.New("abc") == errors.New("abc") {
+		t.Errorf(`New("abc") == New("abc")`)
+	}
+}
+
 func main() {
 	fmt.Println("GitHubActions")
 	fmt.Print("Heelo GitHub Aciotns\n")
